1b7be3971d2d6473e8e3f9f30d32f44ebbe3d5c1
최현정 <hjchoi.dev@gmail.com>
2025-09-23 13:14:43 +0900

    feat: Implement multi-bookmark

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,18 +1,3 @@
-/*
- * Copyright 2025 ChamCoach
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 plugins {
     id("hyunjung.chamcoach.android.application")
     id("hyunjung.chamcoach.android.application.compose")
@@ -20,6 +5,7 @@ plugins {
     id("hyunjung.chamcoach.spotless")
     alias(libs.plugins.kotlin.parcelize)
     alias(libs.plugins.baselineprofile)
+    alias(libs.plugins.kotlinx.serialization)
 }
 
 android {
@@ -75,6 +61,8 @@ dependencies {
 
     // datastore
     implementation("androidx.datastore:datastore-preferences:1.1.1")
+
+    implementation(libs.kotlinx.serialization.json)
 
     // di
     implementation(libs.hilt.android)
